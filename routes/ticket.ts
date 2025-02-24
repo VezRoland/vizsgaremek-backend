eb6c9f3afcdbfaa04588e214500c4f5d74977fb1
Henlorata <matuzabalazs@gmail.com>
2025-02-24 17:02:15 +0100

    fix ticket table name

--- routes/ticket.ts
+++ routes/ticket.ts
@@ -1,13 +1,16 @@
-import { Router } from "express"
+import { Router, type Request } from "express"
 import { object, string } from "zod"
 import postgres from "../lib/postgres.ts"
+import { getUserFromCookie } from "../lib/utils.ts"
+import type { ServerResponse } from "../lib/types/response.ts"
 
 const router = Router()
 
 // Create new ticket
-router.post("/tickets", async (req, res) => {
+router.post("/tickets", getUserFromCookie, async (req: Request, res) => {
 	const schema = object({ title: string(), description: string() })
 	const validation = schema.safeParse(req.body)
+	const user = req.user
 
 	if (!validation.success) {
 		return res.status(400).json({ error: validation.error.message })
@@ -17,11 +20,21 @@ router.post("/tickets", async (req, res) => {
 
 	try {
 		const result = await postgres.query(
-			"INSERT INTO tickets (title, description) VALUES ($1, $2) RETURNING *",
+			"INSERT INTO ticket (title, content) VALUES ($1, $2) RETURNING *",
 			[title, description]
 		)
-		res.status(201).json(result.rows[0])
+		res.status(201).json({
+			error: false,
+			type: "message",
+			messageType: "success",
+			message: "Sikeres hibajegy létrehozás!",
+		} satisfies ServerResponse)
 	} catch (err: any) {
-		res.status(500).json({ error: err.message })
+		res.status(500).json({
+			error: true,
+			type: "message",
+			messageType: "error",
+			message: "Sikertelen hibajegy létrehozás!",
+		} satisfies ServerResponse)
 	}
 })
